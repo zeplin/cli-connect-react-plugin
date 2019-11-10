920e0aaef52caee9fd9c5baae486f0f9d9258c1c
Yucel Okcu <yuquailon@gmail.com>
2019-11-10 20:39:49 +0300

    Separate component templates

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,40 @@ import { parse } from "react-docgen";
 export default class implements ZeplinLinkPlugin {
     supportedFileExtensions = [".js", ".jsx"];
 
-    generateSnippet = pug.compileFile(path.join(__dirname, "template/snippet.pug"));
+    generateSnippet = pug.compileFile(path.join(__dirname, "template/base.pug"));
+    generateSnippetWithChildren = pug.compileFile(path.join(__dirname, "template/withChildren.pug"));
+    generateSnippetWithProps = pug.compileFile(path.join(__dirname, "template/withProps.pug"));
+    generateSnippetWithChildrenAndProps = pug.compileFile(path.join(__dirname, "template/withChildrenAndProps.pug"));
 
     async process(context: ComponentConfig): Promise<ComponentData> {
         const file = await readFile(path.resolve(context.path));
 
         const reactDocs = parse(file);
 
-        const snippet = this.generateSnippet(reactDocs);
+        let hasChildren = false;
+        let hasProps = false;
+
+        if (reactDocs.props) {
+            if (Object.keys(reactDocs.props).some(key => key === "children")) {
+                hasChildren = true;
+            }
+
+            if (Object.keys(reactDocs.props).some(key => key !== "children")) {
+                hasProps = true;
+            }
+        }
+
+        let snippet;
+        if (hasChildren && hasProps) {
+            snippet = this.generateSnippetWithChildrenAndProps(reactDocs);
+        } else if (hasChildren) {
+            snippet = this.generateSnippetWithChildren(reactDocs);
+        } else if (hasProps) {
+            snippet = this.generateSnippetWithProps(reactDocs);
+        } else {
+            snippet = this.generateSnippet(reactDocs);
+        }
+
         // TODO maybe generate a markdown propTable as description?
         const { description } = reactDocs;
 
---
+++ test/samples/ComponentWithProps.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+
+class Foo {}
+
+/** Component description. */
+class MyComponent extends React.Component {
+    render() {
+        return "";
+    }
+}
+// All examples of propTypes listed official documentation
+MyComponent.propTypes = {
+    optionalArray: PropTypes.array,
+    optionalBool: PropTypes.bool,
+    optionalFunc: PropTypes.func,
+    optionalNumber: PropTypes.number,
+    optionalObject: PropTypes.object,
+    optionalString: PropTypes.string,
+    optionalSymbol: PropTypes.symbol,
+    optionalNode: PropTypes.node,
+    optionalElement: PropTypes.element,
+    optionalElementType: PropTypes.elementType,
+    optionalFoo: PropTypes.instanceOf(Foo),
+    optionalEnum: PropTypes.oneOf(["News", "Photos"]),
+    optionalUnion: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Foo)
+    ]),
+    optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+    optionalObjectOf: PropTypes.objectOf(PropTypes.number),
+    optionalObjectWithShape: PropTypes.shape({
+        color: PropTypes.string,
+        fontSize: PropTypes.number
+    }),
+    optionalObjectWithStrictShape: PropTypes.exact({
+        name: PropTypes.string,
+        quantity: PropTypes.number
+    }),
+    requiredFunc: PropTypes.func.isRequired,
+    requiredAny: PropTypes.any.isRequired,
+    customProp: () => {},
+    customArrayProp: PropTypes.arrayOf(() => {}),
+    customObjectOfProp: PropTypes.objectOf(() => {})
+};
+
+export default MyComponent;
