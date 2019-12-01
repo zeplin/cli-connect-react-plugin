25c95b36a00def24b110fdde50f6638c6bbc0507
Yucel Okcu <yuquailon@gmail.com>
2019-12-01 22:05:14 +0300

    Add Flow component tests

---
+++ test/samples/flow/FlowComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+/**
+ * General component description.
+ */
+export default class MyComponent extends React.Component<void, void, void> {
+
+  render(): ReactElement {
+    // ...
+  }
+}
